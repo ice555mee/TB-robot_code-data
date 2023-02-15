dc169a8cfb0fa42e0055d50822b3646469392b9d
ice555mee <siraprapha.d@vistec.ac.th>
2023-02-15 15:27:40 +0700

    commit

--- Test_prediction/newcode_CV_classification_process3.py
+++ Test_prediction/newcode_CV_classification_process3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b73ae3011140064ce278e85eff81fcdceeb5eaabe5224c6ef442a57e8b62353
-size 3417
+oid sha256:57323dae4253a4232da04551d6b0a05fb35d2e4ea8f06ab5997f5386fdea3d2c
+size 3514
